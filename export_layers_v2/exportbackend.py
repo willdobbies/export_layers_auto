13694483563ab3c61e4d09e7802c731117682056
Chris <cjriddle31@gmail.com>
2025-07-05 09:44:19 -0700

    Cleanup code a bit. Add more info to export dialog

--- export_layers_v2/exportbackend.py
+++ export_layers_v2/exportbackend.py
@@ -12,6 +12,7 @@ class ExportBackend():
     exportFilterLayers : bool = False
     ignoreInvisibleLayers : bool = True
     imageFormat : str = "png"
+    nameFormat : str = "{document_name} - {layer_name}"
 
     def mkdir(self, directory):
         target_directory = directory
@@ -31,54 +32,70 @@ class ExportBackend():
         self.height = document.height()
         self.res = document.resolution()
 
+        self.document = document
+
         docPath = document.fileName() if document.fileName() else '~/Untitled/Untitled.kra'
         docPathHead,docPathTail = os.path.split(docPath)
         docName, docExt = os.path.splitext(docPathTail)
+
+        self.document_name = docName
 
         outDir = os.path.join(docPathHead, docName)
 
         self.mkdir(outDir)
 
-        self._exportLayers(
-            document.rootNode(),
-            self.imageFormat,
-            outDir)
+        results = self._exportLayers(document.rootNode(), outDir)
+        result_names = '\n'.join(results)
 
         Application.setBatchmode(True)
 
         popup = QMessageBox()
-        popup.setText(f"Exported layers to {outDir}") 
+        popup.setText(f"Exported {len(results)} layers to {outDir}: \n {result_names}") 
         popup.exec_()
 
-    def _exportLayers(self, parentNode, fileFormat, parentDir):
-        """ This method get all sub-nodes from the current node and export then in
-            the defined format."""
+    def isLayerIgnored(self, node):
+        if (not self.exportFilterLayers and 'filter' in node.type()):
+            return True
+        elif (self.ignoreInvisibleLayers and not node.visible()):
+            return True
+        return False
 
+    def exportLayer(self, node, outname):
+        nodeName = node.name()
+
+        if self.cropToImageBounds:
+            bounds = QRect()
+        else:
+            bounds = QRect(0, 0, self.width, self.height)
+
+        try:
+            node.save(outname, self.res / 72., self.res / 72., krita.InfoObject(), bounds)
+        except Exception as e:
+            print("Export error: {e}")
+            return False
+
+        return True
+
+    def getOutname(self, node):
+        self.nameFormat.replace("{document_name}", self.document_name)
+        self.nameFormat.replace("{layer_name}", node.name())
+
+        return f"{node.name()}.{self.imageFormat}"
+
+    def _exportLayers(self, parentNode, parentDir):
+        results = []
         for node in parentNode.childNodes():
-            newDir = ''
-            if node.type() == 'grouplayer' and not self.groupAsLayer:
+            if(self.isLayerIgnored(node)):
+                continue
+
+            # Recursive make subdirectory + export group layer children
+            if node.type() == 'grouplayer' and not self.groupAsLayer and node.childNodes():
                 newDir = os.path.join(parentDir, node.name())
                 self.mkdir(newDir)
-            elif (not self.exportFilterLayers and 'filter' in node.type()):
-                continue
-            elif (self.ignoreInvisibleLayers and not node.visible()):
-                continue
-            else:
-                nodeName = node.name()
-                _fileFormat = self.imageFormat
-                if '[jpeg]' in nodeName:
-                    _fileFormat = 'jpeg'
-                elif '[png]' in nodeName:
-                    _fileFormat = 'png'
-
-                if self.cropToImageBounds:
-                    bounds = QRect()
-                else:
-                    bounds = QRect(0, 0, self.width, self.height)
-
-                layerFileName = f'{parentDir}/{node.name()}.{_fileFormat}'
-
-                node.save(layerFileName, self.res / 72., self.res / 72., krita.InfoObject(), bounds)
-
-            if node.childNodes() and not self.groupAsLayer:
                 self._exportLayers(node, fileFormat, newDir)
+
+            else:
+                outname = f'{parentDir}/{self.getOutname(node)}'
+                if(self.exportLayer(node, outname)):
+                    results.append(outname)
+        return results
